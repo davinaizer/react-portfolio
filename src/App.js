a1e46a432b1f9238dc9ef1abe4f50c367959e0d7
Davi Naizer <davinaizer@gmail.com>
2018-11-17 19:03:12 -0200

    renamed CSS files to SCSS and added Custom Boostrap build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -11,13 +12,16 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            <Button color="primary" size="lg">
+              Learn React
+            </Button>
           </a>
         </header>
       </div>
