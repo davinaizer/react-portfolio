9935358e820b523968dc8de4282f8430a2bbfd6e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-15 22:41:31 +0000

    Deploy davinaizer/react-portfolio to github.com/davinaizer/react-portfolio.git:gh-pages

---
+++ precache-manifest.9f64cd82c0cf5011c34b320b79426bae.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3603dc302868020e9ac07eec29b211",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c711f741aba7b89ad9c6",
+    "url": "/react-portfolio/static/css/main.60672521.chunk.css"
+  },
+  {
+    "revision": "a7a710f7da34e90cd6a6",
+    "url": "/react-portfolio/static/js/2.e57720c7.chunk.js"
+  },
+  {
+    "revision": "c711f741aba7b89ad9c6",
+    "url": "/react-portfolio/static/js/main.7ebbc3f6.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "eb3750dfe7ba2c79a8733ecd21f75abe",
+    "url": "/react-portfolio/static/media/201810_cv_davi_naizer.eb3750df.pdf"
+  },
+  {
+    "revision": "afebcbcdd1192dae6bacb6c8ec04f24d",
+    "url": "/react-portfolio/static/media/about-bg.afebcbcd.jpg"
+  },
+  {
+    "revision": "3919ae867b6c412248be2b8b2afe628f",
+    "url": "/react-portfolio/static/media/black_noise_50.3919ae86.png"
+  },
+  {
+    "revision": "e6d26983f7849708301e53e65f90e809",
+    "url": "/react-portfolio/static/media/contact-bg.e6d26983.jpg"
+  },
+  {
+    "revision": "c7dfa61a2532db57c0a6cb16f46a3f5c",
+    "url": "/react-portfolio/static/media/file-pdf.c7dfa61a.svg"
+  },
+  {
+    "revision": "eebf9e60ae5465481e8532cef7feda27",
+    "url": "/react-portfolio/static/media/social_envelope-square.eebf9e60.svg"
+  },
+  {
+    "revision": "d5cf4b7f0ed851c53d7a0c5779641431",
+    "url": "/react-portfolio/static/media/social_facebook.d5cf4b7f.svg"
+  },
+  {
+    "revision": "fa5f9f5385014309c11ded0c7fe26811",
+    "url": "/react-portfolio/static/media/social_github.fa5f9f53.svg"
+  },
+  {
+    "revision": "2882d1a1fff0345c3bc3b5c3151daa32",
+    "url": "/react-portfolio/static/media/social_linkedin.2882d1a1.svg"
+  },
+  {
+    "revision": "b204d52be34b7e6cf847a636dc3abfc0",
+    "url": "/react-portfolio/static/media/work-bg.b204d52b.jpg"
+  }
+]);
